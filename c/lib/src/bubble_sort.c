9388152273f4db0bd56258a95eebe1d45cc16e66
Diane Williams <diane.williams@ucdenver.edu>
2026-06-21 17:03:39 -0500

    Rewrote bubble sort. My error in that I originally coded selection sort.

--- c/lib/src/bubble_sort.c
+++ c/lib/src/bubble_sort.c
@@ -2,12 +2,21 @@
 
  void bubble_sort(int *arr, size_t n)
  {
-    for(size_t i = 0; i < n; i++){
-        for(size_t j = i + 1; j < n; j++){
-            if(arr[i] > arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    // Safety check for null array pointer or a small array that is already 
+    // sorted by default. 
+    if (arr == NULL || n < 2){
+        return;
+    }
+
+    // The outer loop controls the maximum number of iterations
+    for(size_t i = 0; i < n - 1; i++){
+        // The inner loop controls which pairs are compared
+        // Always restarts at 0
+        for(size_t j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
